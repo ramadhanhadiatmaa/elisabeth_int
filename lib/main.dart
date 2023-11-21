465adfcecf660655999f236aeb0123df4c98754f
kodlitecom <nandaaprilia605@gmail.com>
2023-11-21 21:07:43 +0700

    ci(dockerfile): Add Dockerfile to Project

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 
 void main() async {
-  await dotenv.load(fileName: '.env');
+  await dotenv.load(/* fileName: '.env' */);
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
